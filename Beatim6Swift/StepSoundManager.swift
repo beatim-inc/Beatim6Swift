c75b25d5459c843f98423ddc4c5b3d52c029eace
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2025-02-19 16:28:38 +0900

    ステップサウンド中に音が止まらないように

--- Beatim6Swift/StepSoundManager.swift
+++ Beatim6Swift/StepSoundManager.swift
@@ -14,6 +14,21 @@ class StepSoundManager {
     var audioPlayer: AVAudioPlayer?
     let soundName = "step_sound"
 
+    
+    init() {
+        setupAudioSession()
+    }
+
+    private func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default, options: [.mixWithOthers])
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Failed to set audio session: \(error)")
+        }
+    }
+    
+    
     func playSound() {
         if let url = Bundle.main.url(forResource: soundName,withExtension:"mp3") {
             do {
